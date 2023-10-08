e6ce74736c13be69919fbf3db84bbc42324cb7c9
Shubhamjadhavrao <28shubhamjadhav@gmail.com>
2023-10-08 13:05:11 +0530

    practice programs

---
+++ JavaPrograms/src/studyAplication/pattern.java
@@ -0,0 +1,20 @@
+package studyAplication;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 4;
+		int m= 5;
+		
+		for(int i=0; i<n ; i++) {
+			for(int j=0 ; j<m; j++) {
+				System.out.print("*");
+			}
+		  System.out.println();
+	}
+}
+}
+         
+
+
