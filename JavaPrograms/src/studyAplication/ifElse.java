57564ef225e71c55befedd3fa0bcfb0368bd8039
Shubhamjadhavrao <28shubhamjadhav@gmail.com>
2023-10-08 18:36:23 +0530

    if else condition

---
+++ JavaPrograms/src/studyAplication/ifElse.java
@@ -0,0 +1,23 @@
+package studyAplication;
+
+import java.util.*;
+
+ public class ifElse {
+	public static void main(String[] args) {
+		
+         int a=5;
+         int b=8;
+		
+
+		if (a > b) {
+			System.out.println("a is Greter");
+		} else 
+			if (a < b) {
+			System.out.println("b is Greter");
+		   } else {
+			System.out.println("a is Smaller");
+		   }
+
+	}
+
+}
