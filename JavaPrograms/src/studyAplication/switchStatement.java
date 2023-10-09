17c4b61a1e29de7d96076bd2fdf45060930128f0
Shubhamjadhavrao <28shubhamjadhav@gmail.com>
2023-10-09 11:02:59 +0530

    Switch case

---
+++ JavaPrograms/src/studyAplication/switchStatement.java
@@ -0,0 +1,38 @@
+package studyAplication;
+
+import java.util.*;
+
+public class switchStatement {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+//		int n = 1;
+		switch (n) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		default:
+			System.out.println("Sunday");
+		}
+
+	}
+
+	
+}
+
