cf8deefb9f994bb4a00a8eecc4e35578f9ab0569
Shubhamjadhavrao <28shubhamjadhav@gmail.com>
2023-10-22 18:51:13 +0530

    change add

---
+++ JavaPrograms/src/Inheritance/SimpleInheritance/student.java
@@ -0,0 +1,11 @@
+package Inheritance.SimpleInheritance;
+
+class student {
+	 int rollNo,marks;
+	 String name;
+	 void input() 
+	{
+		System.out.println("Enter sturdent rollNo and name and marks:");
+	}
+
+}
