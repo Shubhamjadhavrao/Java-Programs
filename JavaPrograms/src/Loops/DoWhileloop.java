4c1e20f73ac827fe1d3c71f8f16261e167829c3f
Shubhamjadhavrao <28shubhamjadhav@gmail.com>
2023-11-23 12:58:49 +0530

    loops

---
+++ JavaPrograms/src/Loops/DoWhileloop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class DoWhileloop {
+	public static void main(String[] args) {
+
+		int i = 1;
+		do  {
+			System.out.println(i);
+			i++;
+		}while(i<=100);
+	}
+}
---
+++ JavaPrograms/src/Loops/infinivtiveDoWhileloop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class infinivtiveDoWhileloop {
+	public static void main(String[] args) {
+		
+	
+	do {
+		System.out.println("infinitive loop");
+		
+	}while(true);
+	}
+}
